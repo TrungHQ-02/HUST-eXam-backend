47c5a926b12f65c791d6f81971536dd46b41e4ea
Hoàng Quốc Trung <103867359+TrungHQ-02@users.noreply.github.com>
2023-04-10 10:08:11 +0700

    init project

---
+++ src/routes/web.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+let router = express.Router();
+
+let initRoutes = (app) => {
+   return app.use("/", router);
+}
+
+module.exports = initRoutes;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initRoutes from "./routes/web";
+import connectDB from "./config/connectDB";
+import cors from 'cors';
+require('dotenv').config();
+
+let app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app config
+viewEngine(app);
+initRoutes(app);
+connectDB();
+
+// run app
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log("Our server is running on port", port);
+})
