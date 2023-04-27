77a5b3bd2f893587ee8dea14768ca5adf734dac5
Hoàng Quốc Trung <103867359+TrungHQ-02@users.noreply.github.com>
2023-04-27 15:04:27 +0700

    准备

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
   if (!authHeader) {
     return res.status(401).json({
       code: "x001",
-      message: "Unauthorized: No token provided",
+      message: "Unauthorized:  No token provided",
     });
   }
 
